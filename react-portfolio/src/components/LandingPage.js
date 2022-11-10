42c8ea60b846ab212a25dc7b8d9df7105043a288
TommyAlv <tommyalvarado2@gmail.com>
2022-11-09 18:03:42 -0700

    Added Typing.js to landing page

--- react-portfolio/src/components/LandingPage.js
+++ react-portfolio/src/components/LandingPage.js
@@ -1,4 +1,12 @@
 import logo from "../assets/tom-alogo.png"
+import React, { useEffect } from 'react';
+import Typed from "react-typed";
+
+const textLines = [
+    `Full Stack Developer`,
+    `Business Analyst`,
+    `Project Manager`,
+];
 
 
 const LandingPage = () => {
@@ -12,10 +20,36 @@ const LandingPage = () => {
                 <div className="m-auto">
                     <div className="mx-auto text-center prose">
                         <div className="container">
-                            <img src={ logo } alt="logo" className="object-cover mx-auto h-36 w-36 rounded-full"
-                                id="avatarPhoto" />
-                            <h1 className="text-4xl font-bold pt-4 px-10 text-slate-600">Tommy Alvarado</h1>
-                            
+                            <h1 className="text-4xl font-bold pt-4 px-10 text-slate-600 ">Tommy Alvarado</h1>
+                            <p className="pt-6 lg:pt-3 md:pt-2 lg:pb-4 sm:pb-9 md:px-4 sm:text-4xl text-3xl font-semibold text-indigo-800 "
+                                id="textRow">
+                                <Typed strings={textLines} startDelay={300} typeSpeed={60} backSpeed={100} backDelay={100} smartBackspace={true} loop={true} />
+                            </p>
+                            {/* <!-- menu button  --> */}
+                            <div className="dropdown mt-4">
+                                <div tabindex="0"
+                                    className="m-1 btn text-slate-300 bg-indigo-800 hover:bg-indigo-400 text-2xl mx-auto"
+                                    id="menuButton"><span className="mx-auto my-auto">Where To Begin...</span></div>
+                                <div className="hidden menu items-center text-center container w-full" id="mobileMenu">
+                                    <ul className="bg-slate-300 shadow-lg shadow-indigo-800 bg-opacity-30 w-full items-center text-center justify-center">
+                                        <li
+                                            className="text-2xl sm:text-lg text-indigo-800 items-center hover:bg-slate-300 hover:bg-opacity-50 hover:text-white"
+                                            id="hLink">
+                                            <a href="./assets/html/aboutme.html">Get To Know Me</a>
+                                        </li>
+                                        <li
+                                            className="text-2xl sm:text-lg text-indigo-800  items-center hover:bg-slate-300 hover:bg-opacity-50 hover:text-white"
+                                            id="hLink">
+                                            <a href='./assets/html/projects.html' className="item-center text-center">See My Work</a>
+                                        </li>
+                                        <li
+                                            className="text-2xl sm:text-lg text-indigo-800 items-center hover:bg-slate-300 hover:bg-opacity-50 hover:text-white"
+                                            id="hLink">
+                                            <a href="./assets/html/contact.html">Reach Out</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
